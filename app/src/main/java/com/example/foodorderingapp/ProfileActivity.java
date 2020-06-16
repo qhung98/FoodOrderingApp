fc1df7a7d6482b68b403703aba582eaef7f707f8
Zhiend <zhiend989@gmail.com>
2020-06-16 11:55:37 +0700

    Change something in ProfileActivity

--- app/src/main/java/com/example/foodorderingapp/ProfileActivity.java
+++ app/src/main/java/com/example/foodorderingapp/ProfileActivity.java
@@ -1,14 +1,54 @@
 package com.example.foodorderingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+    TextView tvUserName, tvUserPhone, tvUserAddress;
+    Button btnChangePassword, btnChangeProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tvUserName = findViewById(R.id.tvUserName);
+        tvUserPhone = findViewById(R.id.tvUserPhone);
+        tvUserAddress = findViewById(R.id.tvUserAddress);
+        btnChangePassword = findViewById(R.id.btnChangePassword);
+        btnChangeProfile = findViewById(R.id.btnChangeProfile);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("KFC");
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, ChangePasswordActivity.class));
+            }
+        });
+
+        btnChangeProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, ChangeProfileActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
