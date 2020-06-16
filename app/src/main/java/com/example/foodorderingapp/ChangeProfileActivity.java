3b5ba975c2a0ebc32fe54877f1b051fccf09c7f8
Zhiend <zhiend989@gmail.com>
2020-06-16 11:49:31 +0700

    Change something in ChangeProfileActivity

---
+++ app/src/main/java/com/example/foodorderingapp/ChangeProfileActivity.java
@@ -0,0 +1,46 @@
+package com.example.foodorderingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ChangeProfileActivity extends AppCompatActivity {
+    EditText edNewName, edNewPhone, edNewAddress;
+    Button btnUpdateProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_profile);
+
+        edNewName = findViewById(R.id.edNewName);
+        edNewPhone = findViewById(R.id.edNewPhone);
+        edNewAddress = findViewById(R.id.edNewAddress);
+        btnUpdateProfile = findViewById(R.id.btnUpdateProfile);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("KFC");
+
+        btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Update_Profile", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
